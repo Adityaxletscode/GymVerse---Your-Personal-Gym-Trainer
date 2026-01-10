300ef48886809b8508d00d7f560aee984fea080a
Adityaxletscode <150706824+Adityaxletscode@users.noreply.github.com>
2026-01-10 21:09:45 +0530

    Updated Render link

--- docs/script.js
+++ docs/script.js
@@ -1,25 +1,25 @@
-const getStarted=document.querySelector('.get-started');
-const learnMore=document.querySelector(".learn-more");
+const getStarted = document.querySelector(".get-started");
+const learnMore = document.querySelector(".learn-more");
 const currentPath = window.location.pathname;
-const navLinks = document.querySelectorAll('.right-container a');
+const navLinks = document.querySelectorAll(".right-container a");
 
-getStarted.addEventListener("click", function() {
-    window.location.href = "http://localhost:4000";
-  });
+getStarted.addEventListener("click", function () {
+  window.location.href = "https://gymverse-backend-rz7f.onrender.com";
+});
 
-learnMore.addEventListener("click", function() {
-    window.location.href = "./info.txt";
-  });
+learnMore.addEventListener("click", function () {
+  window.location.href = "./info.txt";
+});
 
-navLinks.forEach(link => {
-  const href = link.getAttribute('href');
+navLinks.forEach((link) => {
+  const href = link.getAttribute("href");
   console.log(`Comparing: ${href} with ${currentPath}`);
   if (
     href === currentPath ||
-    (currentPath === '/' && href === 'index.html') ||
-    (currentPath.endsWith('/index.html') && href === 'index.html')
+    (currentPath === "/" && href === "index.html") ||
+    (currentPath.endsWith("/index.html") && href === "index.html")
   ) {
-    link.classList.add('active');
+    link.classList.add("active");
     console.log(`Added active to ${href}`);
   }
 });
