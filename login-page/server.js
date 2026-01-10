05e95dedd59e031d9faa52c99194934c13a3c76e
Adityaxletscode <150706824+Adityaxletscode@users.noreply.github.com>
2026-01-10 20:26:27 +0530

    Fix mongoose connection options

--- login-page/server.js
+++ login-page/server.js
@@ -20,10 +20,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 /* ---------- MongoDB Connection ---------- */
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 
