089c45cb57cf4ce5bdf090074638a9b6f33c2f6a
Adityaxletscode <150706824+Adityaxletscode@users.noreply.github.com>
2026-01-11 20:17:04 +0530

    Updated dashboard URL

--- docs/dashboard-page/script.js
+++ docs/dashboard-page/script.js
@@ -1,12 +1,12 @@
-const logOutBtn=document.querySelector('.log-out');
-const welcome=document.querySelector('.welcome')
-const plan=document.querySelector('.card1');
-const chat=document.querySelector('.card2');
-const nutrition=document.querySelector('.card3');
+const logOutBtn = document.querySelector(".log-out");
+const welcome = document.querySelector(".welcome");
+const plan = document.querySelector(".card1");
+const chat = document.querySelector(".card2");
+const nutrition = document.querySelector(".card3");
 
-logOutBtn.addEventListener("click", function() {
-    window.location.href = "../public/index.html";
-  });
+logOutBtn.addEventListener("click", function () {
+  window.location.href = "./index.html";
+});
 
 const params = new URLSearchParams(window.location.search);
 const username = params.get("user");
@@ -21,21 +21,21 @@ if (storedUser) {
   welcome.innerHTML = `Welcome, ${storedUser}!`;
 }
 
-plan.addEventListener('click', function() {
+plan.addEventListener("click", function () {
   window.location.href = `./plan.html?user=${storedUser}`;
 });
 
-chat.addEventListener('click', function() {
+chat.addEventListener("click", function () {
   window.location.href = `./chat.html?user=${storedUser}`;
 });
 
-nutrition.addEventListener('click', function() {
+nutrition.addEventListener("click", function () {
   window.location.href = `./nutrition.html?user=${storedUser}`;
 });
 
-const sidebar = document.querySelector('.left-container');
-const toggleBtn = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector(".left-container");
+const toggleBtn = document.querySelector(".sidebar-toggle");
 
-toggleBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('shrink');
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("shrink");
 });
